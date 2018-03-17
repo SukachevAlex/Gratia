bfb7260d081952728b2329d730e813b4daf3b4ce
SukachevAlex <sukachevalexandr@gmail.com>
2018-03-17 23:19:54 +0300

    make footer, make adaptive footer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,4 +19,12 @@ $(document).ready(function() {
         ]
     });
 
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+
 });
