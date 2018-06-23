c166323881384f92bc8dcf24f9256b5937ecdf49
SukachevAlex <sukachevalex@gmail.com>
2018-06-23 22:15:57 +0300

    change main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -39,5 +39,5 @@ $(document).ready(function() {
 });
 
 $(window).on('load', function() {
-    $('.preloader__wrapper').delay(1500).fadeOut('slow');
+    $('.preloader__wrapper').delay(1000).fadeOut('slow');
 });
